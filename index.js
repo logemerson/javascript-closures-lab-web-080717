7aea1747705e9c2ee6517a1615f216d6d93746d5
github username <logemerson@gmail.com>
2017-09-26 20:20:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-const app = "I don't do much."
+var bumpCounter = () => {
+  let counter = 0
+
+  var addBump = () => {
+    counter++
+  }
+  var getBumps = () => {
+    return counter
+  }
+
+  return {
+    addBump,
+    getBumps
+  }
+}
+
+
+var createAnimal = animalType => {
+  return (function(deadlyDevice) {
+    var diabolicalCreation = {
+      animalType: `${animalType}`,
+      deadlyDevice: `${deadlyDevice}`
+    }
+    return diabolicalCreation
+  })
+}
+
+var sharkCreator = createAnimal("Shark")
+var sharkWithFrickinLaserbeam = sharkCreator("Laserbeam")
+var sharkWithFrickinCannon = sharkCreator("Cannon")
